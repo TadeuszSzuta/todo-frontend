e2aa366b6d36e7ce006a71c3131071e93e8cd9f0
TeddyS <szuta.tadeusz@gmail.com>
2024-11-16 00:17:57 +0100

    some garbage

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import backend_PORT from "./src/api/todoApi";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Możesz zmienić port, jeśli inny jest zajęty
+    port: backend_PORT, // Możesz zmienić port, jeśli inny jest zajęty
     proxy: {
       "/todoHub": {
         target: "http://localhost:7203",
